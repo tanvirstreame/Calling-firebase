54014dd26413b666ef915eddd8f939c4b8e5cd5e
tanvirstreame <engr.tanvir.islam.1@gmail.com>
2024-03-09 03:15:05 +0600

    Add modal if firebase message received

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { getToken } from "firebase/messaging";
+import { firebaseMessaging } from './firebase';
+import { getMessaging, onMessage } from "firebase/messaging";
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [incommingCall, setIncommingCall] = useState({});
+
+  async function requestPermission() {
+    const permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      const token = await getToken(firebaseMessaging, {
+        vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY,
+      });
+      console.log("Token generated : ", token);
+    } else if (permission === "denied") {
+      alert("You denied for the notification");
+    }
+  }
+
+  const messaging = getMessaging();
+  onMessage(messaging, (payload) => {
+    setIncommingCall(payload)
+  });
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {incommingCall?.notification?.title ?
+        <div className={`modal ${incommingCall?.notification?.title ? 'modal-open' : ''}`}>
+          <div className="modal-content">
+            <div>
+              <div>Calling</div>
+              <div>{incommingCall?.notification?.title} is calling you</div>
+            </div>
+          </div>
+        </div>
+        :
+        <></>
+      }
+
     </div>
   );
 }
